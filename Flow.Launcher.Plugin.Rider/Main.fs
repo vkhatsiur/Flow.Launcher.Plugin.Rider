f0bbd25ecd8c2e3c915066f1ea98522d8b71a1cc
Vasyl Khatsiur <vasil.khatsiur@gmail.com>
2025-05-28 21:55:10 +0300

    Initial commit

---
+++ Flow.Launcher.Plugin.Rider/Main.fs
@@ -0,0 +1,16 @@
+﻿namespace Flow.Launcher.Plugin.Rider
+
+open System.Collections.Generic
+open Flow.Launcher.Plugin
+
+type RiderPlugin() =
+    
+    let mutable initContext = PluginInitContext()
+    
+    interface IPlugin with
+    
+        member this.Init(context) = initContext <- context
+        
+        member this.Query(query) = List<Result> [
+            Result(Title = "Hello from rider plugin")
+        ]
