00b9487f97688bdf6e345dddac996fc9ab672dab
Vasyl Khatsiur <vasil.khatsiur@gmail.com>
2025-05-29 21:41:42 +0300

    feat: implement recent Rider solution search

--- Flow.Launcher.Plugin.Rider/Main.fs
+++ Flow.Launcher.Plugin.Rider/Main.fs
@@ -1,16 +1,54 @@
 ﻿namespace Flow.Launcher.Plugin.Rider
 
+open System
 open System.Collections.Generic
+open System.IO
+open System.Xml.Linq
 open Flow.Launcher.Plugin
 
+module RecentSolutionsReader =
+
+    let private extractSolutionsPath (recentSolutionsXmlPath: string) : seq<string> =
+
+        try
+            let xmlDocument = XDocument.Load(recentSolutionsXmlPath)
+
+            xmlDocument.Descendants("entry")
+            |> Seq.choose (fun (element: XElement) -> element.Attribute("key") |> Option.ofObj |> Option.map _.Value)
+        with _ ->
+            Seq.empty // ignore exception for now
+
+    let collectFromDirectory (rootDirectory: string) : seq<string> =
+        Directory.GetDirectories(rootDirectory, "Rider*")
+        |> Array.map (fun riderDirectory -> Path.Combine(riderDirectory, "options", "recentSolutions.xml"))
+        |> Array.filter File.Exists
+        |> Seq.collect extractSolutionsPath
+        |> Seq.distinct
+        |> Seq.filter File.Exists
+
 type RiderPlugin() =
-    
+
     let mutable initContext = PluginInitContext()
-    
+
     interface IPlugin with
-    
+
         member this.Init(context) = initContext <- context
-        
-        member this.Query(query) = List<Result> [
-            Result(Title = "Hello from rider plugin")
-        ]
+
+        member this.Query(query) =
+
+            let applicationDataFolder =
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "JetBrains")
+
+            RecentSolutionsReader.collectFromDirectory applicationDataFolder
+            |> Seq.map (fun solutionPath ->
+                Result(
+                    Title = Path.GetFileNameWithoutExtension(solutionPath),
+                    SubTitle = solutionPath,
+                    IcoPath = initContext.CurrentPluginMetadata.IcoPath,
+                    Action =
+                        (fun _ ->
+                            initContext.API.ShellRun(solutionPath)
+                            true)
+                ))
+            |> Seq.filter _.Title.Contains(query.Search, StringComparison.OrdinalIgnoreCase)
+            |> List<Result>
---
+++ Flow.Launcher.Plugin.Rider/TestHost.fsx
@@ -0,0 +1,9 @@
+﻿#r @"bin/Debug/net7.0-windows/Flow.Launcher.Plugin.Rider.dll"
+
+open System
+open System.IO
+open Flow.Launcher.Plugin.Rider
+
+let applicationDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "JetBrains");
+let files = RecentSolutionsReader.collectFromDirectory applicationDataFolder
+files |> Seq.iter (printfn "%s")
